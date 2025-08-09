7d5384cb4ff66aaa9bde53bdf2990a4f5c23244e
Jacob O. Smith <jacobsmith95@gmail.com>
2025-08-09 14:51:40 -0700

    added second file

--- target/debug/deps/learning_rust-3de5100e3142e934.d
+++ target/debug/deps/learning_rust-3de5100e3142e934.d
@@ -1,6 +1,7 @@
-C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\liblearning_rust-3de5100e3142e934.rmeta: src\lib.rs src\first.rs
+C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\liblearning_rust-3de5100e3142e934.rmeta: src\lib.rs src\first.rs src\second.rs
 
-C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\learning_rust-3de5100e3142e934.d: src\lib.rs src\first.rs
+C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\learning_rust-3de5100e3142e934.d: src\lib.rs src\first.rs src\second.rs
 
 src\lib.rs:
 src\first.rs:
+src\second.rs:
