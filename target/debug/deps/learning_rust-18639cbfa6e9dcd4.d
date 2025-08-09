c2e2803b9ca627eabae66d2d39512df9b95a6a9c
Jacob O. Smith <jacobsmith95@gmail.com>
2025-08-09 14:22:27 -0700

    wrote simple singly-linked linked list data structure

---
+++ target/debug/deps/learning_rust-18639cbfa6e9dcd4.d
@@ -0,0 +1,8 @@
+C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\liblearning_rust-18639cbfa6e9dcd4.rmeta: src\lib.rs src\first.rs
+
+C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\liblearning_rust-18639cbfa6e9dcd4.rlib: src\lib.rs src\first.rs
+
+C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\learning_rust-18639cbfa6e9dcd4.d: src\lib.rs src\first.rs
+
+src\lib.rs:
+src\first.rs:
