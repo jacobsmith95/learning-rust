3076d8a6ac624106bdb2afb7f1443885273ce288
Jacob O. Smith <jacobsmith95@gmail.com>
2025-08-18 10:51:01 -0700

    added basic testing in third dot rs

--- src/third.rs
+++ src/third.rs
@@ -28,4 +28,35 @@ impl<T> List<T> {
     pub fn tail(&self) -> List<T> {
         List { head: self.head.as_ref().and_then(|node| node.next.clone())}
     }
+
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let list = List::new();
+        assert_eq!(list.head(), None);
+
+        let list = list.prepend(1).prepend(2).prepend(3);
+        assert_eq!(list.head(), Some(&3));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&2));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&1));
+
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+    }
+
 }
--- target/debug/deps/learning_rust-5d724fadb8493ae5.d
+++ target/debug/deps/learning_rust-5d724fadb8493ae5.d
@@ -1,7 +1,8 @@
-C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\learning_rust-5d724fadb8493ae5.exe: src\lib.rs src\first.rs src\second.rs
+C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\learning_rust-5d724fadb8493ae5.exe: src\lib.rs src\first.rs src\second.rs src\third.rs
 
-C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\learning_rust-5d724fadb8493ae5.d: src\lib.rs src\first.rs src\second.rs
+C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\learning_rust-5d724fadb8493ae5.d: src\lib.rs src\first.rs src\second.rs src\third.rs
 
 src\lib.rs:
 src\first.rs:
 src\second.rs:
+src\third.rs:
