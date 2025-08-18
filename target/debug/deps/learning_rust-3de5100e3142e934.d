90eee7f02acca48df050e4bbcfeb2799b8a79d99
Jacob O. Smith <jacobsmith95@gmail.com>
2025-08-18 10:42:53 -0700

    started persistent stack in file third dot rs

--- target/debug/deps/learning_rust-3de5100e3142e934.d
+++ target/debug/deps/learning_rust-3de5100e3142e934.d
@@ -1,7 +1,8 @@
-C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\liblearning_rust-3de5100e3142e934.rmeta: src\lib.rs src\first.rs src\second.rs
+C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\liblearning_rust-3de5100e3142e934.rmeta: src\lib.rs src\first.rs src\second.rs src\third.rs
 
-C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\learning_rust-3de5100e3142e934.d: src\lib.rs src\first.rs src\second.rs
+C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\learning_rust-3de5100e3142e934.d: src\lib.rs src\first.rs src\second.rs src\third.rs
 
 src\lib.rs:
 src\first.rs:
 src\second.rs:
+src\third.rs:
--- target/debug/deps/learning_rust-73b11f44e3e50549.d
+++ target/debug/deps/learning_rust-73b11f44e3e50549.d
@@ -1,7 +1,8 @@
-C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\liblearning_rust-73b11f44e3e50549.rmeta: src\lib.rs src\first.rs src\second.rs
+C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\liblearning_rust-73b11f44e3e50549.rmeta: src\lib.rs src\first.rs src\second.rs src\third.rs
 
-C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\learning_rust-73b11f44e3e50549.d: src\lib.rs src\first.rs src\second.rs
+C:\Users\Jacob\Documents\GitHub\learning-rust\target\debug\deps\learning_rust-73b11f44e3e50549.d: src\lib.rs src\first.rs src\second.rs src\third.rs
 
 src\lib.rs:
 src\first.rs:
 src\second.rs:
+src\third.rs:
